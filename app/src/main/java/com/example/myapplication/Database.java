7ab1f4fb5b2104ac1ffba19805392e66fff1b14a
capttwigg <mike@familien-jahn.dk>
2018-07-12 21:33:49 +0200

    started edit json

--- app/src/main/java/com/example/myapplication/Database.java
+++ app/src/main/java/com/example/myapplication/Database.java
@@ -6,6 +6,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -36,11 +39,25 @@ public class Database extends AsyncTask {
       }
 
       in.close();
-      System.out.println("page result: " + sb.toString());
+      System.out.println(json("[{\"temperature\":\"28.12\",\"date\":\"2018-07-12 20:51:07\"}]"));
+      //System.out.println("page result: " + json(sb.toString()));
       return sb.toString();
     } catch (Exception e) {
       e.printStackTrace();
     }
     return "null";
   }
+  private String json(String json){
+    JSONArray obj = null;
+    try {
+      obj = new JSONArray(json);
+      JSONObject jsonObject  = obj.getJSONObject(0);
+      return  jsonObject.get("date").toString();
+
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
 }
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.util.concurrent.ExecutionException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
   private TextView textView;
 
   @Override
